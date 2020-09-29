716b88780658bb34d516596a77654c0b9ec8e7e9
Robert B <robertb@lifesciencepublishers.com>
2020-09-29 15:26:49 -0600

    JS DOM

--- js/script.js
+++ js/script.js
@@ -1,13 +1,36 @@
+const currencyOne = document.getElementById('currency-one');
+const currencyTwo = document.getElementById('currency-two');
+const amountOne = document.getElementById('amount-one');
+const amountTwo = document.getElementById('amount-two');
+const rate = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
 async function caclulate() {
-    const apiURL = `https://api.exchangerate-api.com/v4/latest/USD`;
+    const currentcy_one = currencyOne.value;
+    const currentcy_two = currencyTwo.value;
+    const apiURL = `https://api.exchangerate-api.com/v4/latest/${currentcy_one}`;
 
     try {
         const res = await fetch(apiURL);
         const data = await res.json();
-        console.log(data);
+        const ex_rate = amountOne.value * data.rates[currentcy_two];
+        rate.textContent = `1 ${currentcy_one} = ${ex_rate} ${currentcy_two}`;
+        amountTwo.value = ex_rate.toFixed(2);
     } catch (error) {
         console.log(error);
     }
 }
+
+currencyOne.addEventListener('change', caclulate);
+currencyTwo.addEventListener('change', caclulate);
+amountOne.addEventListener('input', caclulate);
+amountTwo.addEventListener('input', caclulate);
+
+swap.addEventListener('click', () => {
+    const temp = currencyOne.value;
+    currencyOne.value = currencyTwo.value;
+    currencyTwo.value = temp;
+    caclulate();
+  });
 
 caclulate()
